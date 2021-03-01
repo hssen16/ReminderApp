d9edd6baa6a6b57043e7ab9e90748d0ad713c4a9
Halil Semih ŞEN <halil.semih.sen@hotmail.com>
2021-03-01 15:21:18 +0300

    1 Mart 2. Commit

---
+++ DataAccess/Concrete/EntityFramework/Contexts/ReminderDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class ReminderDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)MSSQLLocalDB;Database=ReminderDb;Trusted_Connection=true");
+        }
+
+        public DbSet<Reminder> Reminders { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ Entities/DTOs/ReminderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+
+namespace Entities.DTOs
+{
+    public class ReminderDetail:IDto
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Priority { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
